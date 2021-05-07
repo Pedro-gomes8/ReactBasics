f57f4399ca7522243314eac5448330578f13ac05
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-05-06 21:37:32 -0300

    Starting from scratch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import './index.css'
+// Nested Components
+function BookList() {
+  return <>
+    <section className='mybooks'>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+     </section>
+    </>  
+}
+const Book =() => {
+  return <article>
+    <Image/>
+    <Name/>
+    <Author/>
+  </article>
+}
+const Image =() => <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt="ue"/>
+
+const Name = () =>{
+  return <h4 className='title'>I love you to the moon and back </h4>
+}
+
+const Author =() => {
+  return <p id='autor'> Pedro Gomes</p>
+}
+
+ReactDom.render(<BookList/>,document.getElementById('root'))
